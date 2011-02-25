1bdccd43c4c143f0b9aec5079f336f04beaa28d5
Carlo Alberto Ferraris <cafxx@strayorange.com>
2011-02-25 13:43:30 +0100

    More work (4)

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-This is the dsm add-on.  It contains:
+# dsm
+Distributed Services for Mozilla
 
-* A program (lib/main.js).
-* A few tests.
-* Some meager documentation.
+The dsm library can be used in Firefox Jetpack extensions to perform peer-to-peer asynchronous communications.
+It's currently in the first implementation phases so it's mostly non working.
+
+## Example usage (when it will be ready)
+The following code creates the endpoint "helloWorld" and establishes a connection to a peer with IP peerIP and port peerPort and sends the message "Hello". The peer, upon receiving the message replies with "World".
+
+    const dsm = require("dsm");
+    
+    const ep = new dsm.endpoint("helloWorld", function(msg) {
+        // this function gets called every time this endpoint receives a message
+        if (msg.getData() == "Hello")
+            msg.reply("World");
+    });
+    
+    var c = new dsm.connection(peerIP, peerPort, function() {
+        // this function gets called once the connection has been established
+        ep.sendMessage("Hello", this.getRemoteId());
+    });
+
+The first parameter to sendMessage and reply is the payload to be sent. It can be any JSON-serializable object.
+
+The second parameter to sendMessage is optional and is used to specify which peer will be sent the message. If it is a string it is interpreted as a peerId. If it is an array it is interpreted as a list of peerIds. If it is an integer it is interpreted as a number of random peers. If missing (undefined, null or false), the message will be sent to all currently connected peers. If true, the message will be sent to all known peers.
+
+sendMessage and reply also have a last callback parameter (not shown in the example above) where you can supply a function to be called either on succesful or failed transmission.
+
--- tests/test-main.js
+++ tests/test-main.js
@@ -1,4 +1,4 @@
-const main = require("main");
+const dsm = require("dsm");
 
 exports.test_test_run = function(test) {
   test.pass("Unit test running!");
@@ -8,25 +8,52 @@ exports.test_id = function(test) {
   test.assert(require("self").id.length > 0);
 };
 
-exports.test_url = function(test) {
-  require("request").Request({
-    url: "http://www.mozilla.org/",
-    onComplete: function(response) {
-      test.assertEqual(response.statusText, "OK");
-      test.done();
-    }
-  }).get();
-  test.waitUntilDone(20000);
+exports.test_NodeId = function(test) {
+  test.assert(dsm.controller.getNodeId().length > 0);
 };
 
-exports.test_open_tab = function(test) {
-  const tabs = require("tabs");
-  tabs.open({
-    url: "http://www.mozilla.org/",
-    onReady: function(tab) {
-      test.assertEqual(tab.url, "http://www.mozilla.org/");
-      test.done();
-    }
-  });
-  test.waitUntilDone(20000);
+exports.test_sendMessageStringToSelf = function(test) {
+  var ep = new dsm.messageterminal("test");
+  test.assert(ep != null);
+  test.assert(ep.getMessage() == false);
+  const msg = "test message";
+  ep.sendMessage(msg, dsm.controller.getNodeId());
+  test.assert(ep.getMessage().getData() == msg);
+  test.assert(ep.getMessage() == false);
+  ep.close();
+};
+
+exports.test_sendMessageObjectToSelf = function(test) {
+  var ep = new dsm.messageterminal("test");
+  test.assert(ep != null);
+  const msg = Date.now();
+  ep.sendMessage(msg, dsm.controller.getNodeId());
+  test.assert(ep.getMessage().getData() == msg);
+  test.assert(ep.getMessage() == false);
+  ep.close();
+};
+
+exports.test_sendMessageStringToSelfAndReply = function(test) {
+  var ep = new dsm.messageterminal("test");
+  test.assert(ep != null);
+  const msg = "test message";
+  ep.sendMessage(msg, dsm.controller.getNodeId());
+  const msg2 = "test reply";
+  ep.getMessage().reply(msg2);
+  test.assert(ep.getMessage().getData() == msg2);
+  test.assert(ep.getMessage() == false);
+  ep.close();
+};
+
+exports.test_sendMessageStringToSelfMulti = function(test) {
+  var ep = new dsm.messageterminal("test");
+  var ep2 = new dsm.messageterminal("test");
+  test.assert(ep != null);
+  const msg = "test message";
+  ep.sendMessage(msg, dsm.controller.getNodeId());
+  test.assert(ep.getMessage().getData() == msg);
+  test.assert(ep2.getMessage().getData() == msg);
+  test.assert(ep.getMessage() == false);
+  test.assert(ep2.getMessage() == false);
+  ep.close();
 };
